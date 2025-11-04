0406e5c329e68e65791cb0f0dfb9b891aaed080a
gda-conc <gabrieldaconceicaodeoliveira@gmail.com>
2025-11-04 11:58:53 -0300

    cpp02 ex00 pronto

---
+++ cpp_02/ex00/Fixed.cpp
@@ -0,0 +1,41 @@
+#include "Fixed.hpp"
+
+const int Fixed::_frac_bits = 8;
+
+Fixed::Fixed(void)
+{
+	_value = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Fixed &Fixed::operator=(Fixed const &other)
+{
+	std::cout << "Assignment operator called" << std::endl;
+	if (this != &other)
+	{
+		this->_value = other.getRawBits();
+	}
+	return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_value;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->_value = raw;
+}
