6b1fd0505d35346702cf7e1b77ac28c16fc44928
gabriel <gabrieldaconceicaodeoliveira@gmail.com>
2025-11-06 23:11:56 -0300

    to com muito sono, terminane amnha

---
+++ cpp_03/ex00/ClapTrap.cpp
@@ -0,0 +1,99 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(void)
+{
+	std::cout << "ClapTrap created" << std::endl;
+	this->_hitPoints = 10;
+	this->_energyPoints = 10;
+	this->_attackDamage = 0;
+}
+
+ClapTrap::ClapTrap(std::string const &name)
+{
+	std::cout << "ClapTrap created" << std::endl;
+	this->_name = name;
+}
+
+ClapTrap::~ClapTrap(void)
+{
+	std::cout << "ClapTrap destroyed" << std::endl;
+}
+
+ClapTrap::ClapTrap(ClapTrap const &copy)
+{
+	*this = copy;
+}
+
+ClapTrap &ClapTrap::operator=(ClapTrap const &copy)
+{
+	if(this != &copy)
+	{
+		this->_name = getName();
+		this->_hitPoints = getHitPoints();
+		this->_energyPoints = getEnergyPoints();
+		this->_attackDamage = getAttackDamage();
+	}
+	return *this;
+}
+
+void ClapTrap::setName(std::string name)
+{
+	this->_name = name;
+}
+
+void ClapTrap::setHitPoints(unsigned int num)
+{
+	this->_hitPoints = num;
+}
+void ClapTrap::setEnergyPoints(unsigned int num)
+{
+	this->_energyPoints= num;
+}
+void ClapTrap::setAttackDamage(unsigned int num)
+{
+	this->_attackDamage = num;
+}
+
+std::string ClapTrap::getName()
+{
+	return this->_name;
+}
+int ClapTrap::getHitPoints()
+{
+	return _hitPoints;
+}
+int ClapTrap::getEnergyPoints()
+{
+	return _energyPoints;
+}
+
+int ClapTrap::getAttackDamage()
+{
+	return this->_attackDamage;
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+	std::cout << "ClapTrap "<<this->getName()
+	<<" attacks "<< target
+	<<", causing "<<this->getAttackDamage()
+	<<" points of damage!";
+
+}
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	std::cout << "ClapTrap "<<this->getName()
+	<<" was attacked and took "<< amount
+	<<"of damage."<<this->getAttackDamage()
+	<<" points of damage!";
+
+	this->_hitPoints - amount;
+}
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	std::cout << "ClapTrap "<<this->getName()
+	<<" restored "<< amount
+	<<"of damage."<<this->getAttackDamage()
+	<<" points of damage!";
+	this->_hitPoints + amount;
+}
