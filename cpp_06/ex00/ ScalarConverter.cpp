2e5be39e2af18769fb86da128b6f15b3c025feb5
gabriel <gabrieldaconceicaodeoliveira@gmail.com>
2026-01-14 19:05:26 -0300

    feat(ex00): :sparkles: criação da classe ScalarConvert
    criei a classe scalar convert para e deixei ele não instanciavel

---
+++ cpp_06/ex00/ ScalarConverter.cpp
@@ -0,0 +1,43 @@
+#include " ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter(void){}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &copy)
+{
+	*this = copy;
+}
+
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other)
+{
+	(void)other;
+	return *this;
+}
+
+ScalarConverter::~ScalarConverter(void){}
+
+void ScalarConverter::convert(const std::string &str)
+{
+	size_t	len = str.length();
+	e_type	type = whichType(str, len);
+	switch (type)
+	{
+		case INVALID:
+			std::cout << "Invalid input" << std::endl;
+			break;
+		case SPECIAL:
+			printSpecial(str);
+			break;
+		case CHAR:
+			convertChar(str, len);
+			break;
+		case INT:
+			convertInt(str);
+			break;
+		case FLOAT:
+			convertFloat(str);
+			break;
+		case DOUBLE:
+			convertDouble(str);
+			break;
+	}
+}
---
+++ cpp_06/ex00/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "FOi" << std::endl;
+	return 0;
+}
