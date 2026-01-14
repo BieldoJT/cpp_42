913a9719f39158819a75766c7b36fe0c03f8251f
gabriel <gabrieldaconceicaodeoliveira@gmail.com>
2026-01-14 19:23:48 -0300

    feat(ex00): :sparkles: criação do convert e do printador do especial
    criei uma função pra printar numeros especiais

---
+++ cpp_06/ex00/converters.cpp
@@ -0,0 +1,28 @@
+#include "ScalarConverter.hpp"
+
+void	printSpecial(const std::string &str)
+{
+	if (str == "nan" || str == "nanf")
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: nanf" << std::endl;
+		std::cout << "double: nan" << std::endl;
+	}
+	else if (str == "+inf" || str == "+inff")
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: +inff" << std::endl;
+		std::cout << "double: +inf" << std::endl;
+	}
+	else if (str == "-inf" || str == "-inff")
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: -inff" << std::endl;
+		std::cout << "double: -inf" << std::endl;
+	}
+}
+
+
