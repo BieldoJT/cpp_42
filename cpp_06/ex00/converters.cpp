7f32cfc44658f241ef48358225bf415e2bc0b57b
gabriel <gabrieldaconceicaodeoliveira@gmail.com>
2026-01-15 13:39:29 -0300

    feat(ex00): :sparkles: criei o conversor de double
    crei o conversor de  double para ser printado

--- cpp_06/ex00/converters.cpp
+++ cpp_06/ex00/converters.cpp
@@ -97,4 +97,35 @@ void	convertFloat(const std::string &str)
 	std::cout << "double: " << static_cast<double>(f) << (limit ? ".0" : "") << std::endl;
 }
 
+void	convertDouble(const std::string &str)
+{
+	double	d = std::atof(str.c_str());
+	bool	limit = std::fabs(d - static_cast<int>(d)) < 0.0000000000001;
 
+	std::cout << "char: ";
+	if (d < 0 || d > 127)
+		std::cout << "impossible" << std::endl;
+	else
+	{
+		if (isprint(d))
+			std::cout << "'" << static_cast<char>(d) << "'" << std::endl;
+		else
+			std::cout << "Non displayable" << std::endl;
+	}
+	std::cout << "int: ";
+	if (d < MIN_INT || d > MAX_INT)
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << static_cast<int>(d) << std::endl;
+	std::cout << "float: ";
+	if (d < MIN_FLOAT || d > MAX_FLOAT)
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << static_cast<float>(d) << (limit ? ".0f" : "f") << std::endl;
+	std::cout << "double: ";
+	if (d < MIN_DOUBLE || d > MAX_DOUBLE)
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << d << std::endl;
+
+}
