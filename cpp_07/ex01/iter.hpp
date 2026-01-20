b76be740aab9102146e940d1ddbec01730b21df9
gabriel <gabrieldaconceicaodeoliveira@gmail.com>
2026-01-20 19:26:11 -0300

    fix(ex01): :bug: agora aceita const na função iter
    eu apenas criei um outro template que aceita const e faz as operações corretas, no caso retonar esse se a variavel const for alterada

--- cpp_07/ex01/iter.hpp
+++ cpp_07/ex01/iter.hpp
@@ -5,11 +5,18 @@
 # include <iostream>
 
 template <typename T, typename Func>
-void iter(T* array, const std::size_t& len,const Func func)
+void iter(T* array, const std::size_t& len, Func func)
 {
 	for (size_t i = 0; i < len; i++)
 	{
 		func(array[i]);
+	}
+}
+
+template <typename T, typename Func>
+void iter(const T* array, std::size_t len, Func func) {
+	for (std::size_t i = 0; i < len; ++i) {
+		func(array[i]); // const T&
 	}
 }
 
